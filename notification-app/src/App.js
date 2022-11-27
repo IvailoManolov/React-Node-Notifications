8db77768f3f95590d381ef1d1a967f076d1c6208
Ivaylo <ivomoreras@abv.bg>
2022-11-27 22:30:45 +0200

    socket io server

--- notification-app/src/App.js
+++ notification-app/src/App.js
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Card from './components/card/Card';
 import Login from './components/Login';
 import Navbar from './components/Navbar/Navbar';
+import {io} from "socket.io-client"
 
 import {posts} from "./data"
 
@@ -10,14 +11,24 @@ function App() {
 
   const [userName,setUsername] = useState("")
   const [user,setUser] = useState("")
+  const [socket,setSocket] = useState(null)
+
+  useEffect(() => {
+    setSocket(io("http://localhost:5000"))
+
+  },[])
+
+  useEffect(()=>{
+    socket?.emit("newUser",user)
+  },[socket,user])
 
   return (
     <div className='container'>
       {user ? (
         <>
-          <Navbar />
+          <Navbar socket = {socket}/>
           {posts.map(post => (
-            <Card key = {post.id} post={post} />
+            <Card key = {post.id} post = {post} socket = {socket} user = {user}/>
           ))}
           <span className='username'>{user}</span>
         </>
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const { Server } = require("socket.io");
+
+const io = new Server({
+    cors : {
+        origin:"http://localhost:3000"
+    }
+});
+
+let onlineUsers = []
+
+const addNewUser = (userName, socketId) => {
+    !onlineUsers.some((user) => user.userName === userName) &&
+         onlineUsers.push({userName,socketId})
+}
+
+const removeUser = (socketId) => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId)
+}
+
+const getUser = (userName) => {
+    return onlineUsers.find((user) => user.userName === userName)
+}
+
+io.on("connection", (socket) => {
+    console.log('\x1b[42m%s\x1b[0m',"[SUCESS] Connection found")
+
+    socket.on("newUser",(userName) => {
+        addNewUser(userName, socket.id)
+    })
+
+    io.emit("firstEvent", "Hello this is a test!")
+
+    socket.on("disconnect", () => {
+        console.log('\x1b[42m%s\x1b[0m',"[SUCESS] Connection dropped")
+        removeUser(socket.id)
+    })
+});
+
+io.listen(5000)
